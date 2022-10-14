61729c3a28f815c62232a1a0c316207becf7350d
ArtemB1 <98298807+ArtemB1@users.noreply.github.com>
2022-10-14 10:20:23 +0500

    Д/З JavaScript

---
+++ script.js
@@ -0,0 +1,26 @@
+///Задание №1
+
+function sum(a) {
+  console.log('Верно');
+}
+
+sum(1);
+
+
+///Задание №5
+
+for (let i = 45; i <=65; i++) {
+  console.log(i)
+}
+
+///Задание №2 
+
+function sum2(a) {
+  if (a<0) {
+    console.log('Меньше нуля');
+  } else {
+    console.log('Больше нуля');
+  }
+}
+
+sum2(1);
